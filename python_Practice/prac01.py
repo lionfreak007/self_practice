8fe8713075f82878d2d8c73535bc0ea6d867c293
syed mohammad hamza <syedmohammadhamza@syeds-MacBook-Pro.local>
2020-04-29 12:46:14 +0530

    First commit

--- python_Practice/prac01.py
+++ python_Practice/prac01.py
@@ -1,2 +1,27 @@
+num = 15
+num2 = 20
+
+num3 = num + num2
+print("sum of {0} and {1} is {2} " .format(num,num2,num3))
+
+print("===================")
+"""
+a = input("First Number " )
+b = input ("Second Number" )
+
+sum = float(a) + float(b)
+print("Sum of {0} and {1} is {2}" .format(a,b,sum))
+"""
+a1 = 15
+a2 = 20
+
+sum1 = a1+a2
+print (sum1)
+
+z1 =100
+z2 = 200
+
+sum = z1 + z2
+print("sum of {0} and {1} is {2}" .format(z1,z2,sum))
 
 
